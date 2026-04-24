019caca3d0df9902543c4a91407d52d6d927c2bf
Elya-autho <filippova.elvina2@gmail.com>
2026-04-24 17:19:15 +0500

    add

--- tests/tests_ui/test_first.py
+++ tests/tests_ui/test_first.py
@@ -2,12 +2,34 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-def test_name_field(self):
+def test_name_field():
 
     driver = webdriver.Chrome()
 
-    driver.get('https://lms.threadqa.ru/xpath-practice-hub')
+    driver.get('https://lms.threadqa.ru/xpath-practice-hub/basics')
     time.sleep (3)
+
     #ищем элемент по data-testing="username-field"
-    driver.find_element((By.))
+    name_field = driver.find_element(By.CSS_SELECTOR, "[data-testid='username-field']")
+    assert name_field.is_displayed()
+
+    placeholder = name_field.get_attribute("placeholder")
+
+    assert placeholder == "Введите ваше имя"
+
+    name_field.clear()
+    time.sleep(2)
+
+    name_field.send_keys("Филиппова Эльвина")
+    time.sleep(2)
+
+    name_field.clear()
+    time.sleep(2)
+
+    name_field.send_keys("Смирнов тест")
+    time.sleep(2)
+
+    assert name_field.get_attribute("value") == "Смирнов тест"
+    time.sleep(2)
+
     driver.quit()
